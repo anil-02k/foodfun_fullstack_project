07d962c640a7b0bddecfb0bb774750b71cbf1ada
Anil Kumar <anil582905@gmail.com>
2024-09-09 06:51:49 +0530

    :update 3

--- Frontend/src/pages/home/home.jsx
+++ Frontend/src/pages/home/home.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import './home.css';
 import Header from "../../components/header/header";
 import ExploreMenu from "../../components/exploreMenu/exploreMenu";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 
 const home = () => {
  
@@ -12,7 +13,7 @@ const [category,setCategory]=useState("All");
     <div>
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/> 
-      
+      <FoodDisplay category={category}/>
 
     </div>
   );
